d5c13b650a3b9b0f648b06304c1bf9b7cd9a6876
nhan0612 <trandinhnhan9958@gmail.com>
2019-10-01 17:56:58 +0700

    1/10/2019

--- ConsoleApplication2/ConsoleApplication2/Properties/AssemblyInfo.cs
+++ ConsoleApplication2/ConsoleApplication2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("ConsoleApplication2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("ConsoleApplication2")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e860bc5b-74f5-4a6a-9361-62c152427b26")]
+[assembly: Guid("e885bfbf-130a-46a3-8bbf-e5b9d147a39b")]
 
 // Version information for an assembly consists of the following four values:
 //
